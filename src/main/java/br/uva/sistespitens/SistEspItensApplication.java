bd0b07d0586030194f2fc707d2b5fee500da2438
Vinicius Gonçalves <vfg2006@gmail.com>
2018-09-17 00:50:20 -0300

    Primeiro Commit

---
+++ src/main/java/br/uva/sistespitens/SistEspItensApplication.java
@@ -0,0 +1,12 @@
+package br.uva.sistespitens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistEspItensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistEspItensApplication.class, args);
+	}
+}
